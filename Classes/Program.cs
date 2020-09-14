0d56304848d6c89b18f4c24e398fbd2b63cd3b9b
Александр Минайчев <alexander.minaychev@gmail.com>
2020-09-14 12:02:00 +0700

    Upload of study examples: Classes

---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classes
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() { name = "Неизвестно"; age = 18; }
+
+        public Person(string n) { name = n; age = 18; }
+
+        public Person(string n, int a) { name = n; age = a; }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person();
+            Person bob = new Person("Bob");
+            Person sam = new Person("Sam", 25);
+
+            sam.GetInfo();
+            tom.GetInfo();
+            bob.GetInfo();
+
+        }
+    }
+}
