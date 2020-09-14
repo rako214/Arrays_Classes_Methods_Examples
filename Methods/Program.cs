63afa5423baf19ed9b9579488a419a1580456249
Александр Минайчев <alexander.minaychev@gmail.com>
2020-09-14 12:15:54 +0700

    Upload study examples: methods

---
+++ Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа для сложения");
+            int firstNumber = inputNumber(1);
+            int secondNumber = inputNumber(2);
+            Console.WriteLine("Сумма: " + Sum(firstNumber,secondNumber));
+        }
+
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int inputNumber(int x)
+        {
+            Console.WriteLine($"Введите {x} число: ");
+            return Int32.Parse(Console.ReadLine());
+        }
+    }
+}
